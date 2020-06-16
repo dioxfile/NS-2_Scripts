df81159f158dde1020805aaaf2848ca68d0ccb98
Diógenes Antonio Marques José <32777186+dioxfile@users.noreply.github.com>
2020-06-16 18:56:54 -0400

    Add files via upload

---
+++ Chapter_7_Protocols/olsr-default.tcl
@@ -0,0 +1,7 @@
+# Defaults defined for OLSR
+Agent/OLSR set debug_                false
+Agent/OLSR set willingness_          3
+Agent/OLSR set hello_ival_           2
+Agent/OLSR set tc_ival_              5
+Agent/OLSR set mid_ival_             5
+Agent/OLSR set tc_redundancy_        3
