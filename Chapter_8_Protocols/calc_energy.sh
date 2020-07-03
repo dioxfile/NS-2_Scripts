6b2237f2e94a51fb62de419789d6f4b93714b6ae
Diógenes Antonio Marques José <32777186+dioxfile@users.noreply.github.com>
2020-07-03 17:07:17 -0400

    Update calc_energy.sh

--- Chapter_8_Protocols/calc_energy.sh
+++ Chapter_8_Protocols/calc_energy.sh
@@ -1,17 +1,32 @@
-#!/bin/bash
+#!/bin/dash
+FILE="$1"
+NODE="$2"
+if [ -z "$FILE" ]; then
+  echo "USAGE: ./calc_energy.sh <FILE.tr> <NODE_N>"
+  exit 1
+fi
+if [ -z "$NODE" ]; then  
+  echo "USAGE: ./calc_energy.sh <FILE.tr> <NODE_N>"
+  exit 1
+fi
+NODE_N=`expr $NODE - 1`
+rm -r Energy/
 mkdir -pv Energy
-cat Trace_Cleaned_DSR.tr | sed 's/\[//g' | sed 's/\]//g' | sed 's/\:/ /g' \
-| awk -F" " '{ {if($2 <= 60.000000000) {print}}}' > Trace_Cleaned_Sujo.tr 
-cat Trace_Cleaned_Sujo.tr | uniq > Trace_Cleaned.tr
-egrep "^N.*" Trace_Cleaned.tr > Energy/Energia_total.e 
-cat Energy/Energia_total.e | awk -F" " '{print $5 " " $7 " " $3}' > \
-Energy/Energia_total_col_nodo_energy.e 
-for conta in $(seq 0 59); do
-cat Energy/Energia_total_col_nodo_energy.e  | \
-awk -F" " '{if($1=="'$conta'"){print $3 " " $2}}' > \
-Energy/Energia_total_$conta.e 
-cat Energy/Energia_total_$conta.e | \
-awk -F" " 'END { print (100.000000 - $2) }' \
-> Energy/Energia_Final_$conta.e
-cat  Energy/Energia_Final_$conta.e >> Energy/Energia_Final_Geral.e
+cat $FILE | sed 's/\[//g' | sed 's/\]//g' | sed 's/\_//g' \
+| sed 's/\:/ /g' > Trace_Cleaned.tr
+for conta in $(seq 0 $NODE_N); do
+egrep "^[sr].*" Trace_Cleaned.tr | awk -F" " '{\
+if($3=="'$conta'"){{print $14 }}}' > Energy/Energia_All_by_$conta.e
+cat Energy/Energia_All_by_$conta.e | awk 'END{ print 100-$1 }' > \
+Energy/E_Consumption_by_Node_$conta.e
+cat Energy/E_Consumption_by_Node_$conta.e >> Energy/Average_Node.e   
 done;
+cat Energy/Average_Node.e | awk '{
+Vetor_media[NR] = $0
+} END {
+	for(m = 0; m <= NR; m++){
+	soma = soma + Vetor_media[m]	
+	}
+		media = (soma/NR)
+		printf("%3.9f",media)
+}' > Energy/Energy_Average.tr
