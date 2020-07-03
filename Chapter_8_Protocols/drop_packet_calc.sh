9c7c27746523313135e48fce2acdf484727c39c3
Diógenes Antonio Marques José <32777186+dioxfile@users.noreply.github.com>
2020-07-03 17:08:37 -0400

    Update drop_packet_calc.sh

--- Chapter_8_Protocols/drop_packet_calc.sh
+++ Chapter_8_Protocols/drop_packet_calc.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
+#!/bin/dash
+FILE="$1"
+if [ -z "$FILE" ]; then
+  echo "USAGE: ./drop_packet_calc.sh <FILE.tr>"
+  exit 1
+fi
+rm -r Packet_Loss/
 mkdir -pv Packet_Loss
-cat TRACE_FILE.tr| sed 's/\[//g' | sed 's/\]//g' | sed 's/\_//g' \
-| sed 's/\:/ /g' \
-| awk -F" " '{ {if($2 <= 60.000000000) {print}}}' > Trace_Cleaned_Sujo.tr 
-cat Trace_Cleaned_Sujo.tr | uniq > Trace_Cleaned.tr
-egrep "^[sr].*AGT.*" Trace_Cleaned.tr > Trace_R_S.tr 
+cat $FILE | sed 's/\[//g' | sed 's/\]//g' | sed 's/\_//g' \
+| sed 's/\:/ /g' > Trace_Cleaned.tr
+egrep "^[sr].*AGT" Trace_Cleaned.tr > Trace_R_S.tr 
 cat Trace_R_S.tr | awk -F " " '{   
 	if($1 == "s" && $4 == "AGT"){
 		{print}		
