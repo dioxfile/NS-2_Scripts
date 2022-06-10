2d9e6a001e250e29842fec4d30ee52f647bba9e0
Diógenes Antonio Marques José <32777186+dioxfile@users.noreply.github.com>
2022-06-09 23:27:35 -0400

    Delete olsr-extension.tcl

--- Chapter_5_Setup_Static_Wi-Fi/olsr-extension.tcl
+++
@@ -1,5 +0,0 @@
-Agent/OLSR set debug_              false
-Agent/OLSR set willingness_         3    ;# Default (as published in RFC 3626)
-Agent/OLSR set tc_ival_             5    ;# Default (as published in RFC 3626)
-Agent/OLSR set hello_ival_          2    ;# Default (as published in RFC 3626)
-Agent/OLSR set tc_redundancy_       3    ;# Default (as published in RFC 3626)
