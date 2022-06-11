2118887260a2b719aa5aa7825dc5bc21e28866ed
Diógenes Antonio Marques José <32777186+dioxfile@users.noreply.github.com>
2022-06-11 10:06:36 -0400

    Update olsr-extension.tcl

--- Chapter_6_Set_Wireless_MANET/olsr-extension.tcl
+++ Chapter_6_Set_Wireless_MANET/olsr-extension.tcl
@@ -1,5 +1,5 @@
-Agent/OLSR set mpr_algorithm_               1    ;# 1 = RFC 3626 (as published in RFC 3626)
-Agent/OLSR set routing_algorithm_           1    ;# OLSR Routing = 1 (as published in RFC 3626)
-Agent/OLSR set willingness_         	      3    ;# Default (as published in RFC 3626)
-Agent/OLSR set tc_ival_                     5    ;# Default (as published in RFC 3626)
-Agent/OLSR set hello_ival_            	    2    ;# Default (as published in RFC 3626)
+Agent/OLSR set mpr_algorithm_               1    ;# 1 = RFC 3626 (as published in RFC 3626).
+Agent/OLSR set routing_algorithm_           1    ;# OLSR Routing = 1 (as published in RFC 3626).
+Agent/OLSR set willingness_         	      3    ;# Default (as published in RFC 3626).
+Agent/OLSR set tc_ival_                     5    ;# Default (as published in RFC 3626).
+Agent/OLSR set hello_ival_            	    2    ;# Default (as published in RFC 3626).
