f97660900ac07d928871551a4abfd68345b315e1
Diógenes Antonio Marques José <32777186+dioxfile@users.noreply.github.com>
2022-06-14 17:42:10 -0400

    Update auto_mobility.sh

--- Chapter_6_Set_Wireless_MANET/auto_mobility.sh
+++ Chapter_6_Set_Wireless_MANET/auto_mobility.sh
@@ -1,14 +1,7 @@
 #!/bin/dash
-NN="$1"
-SP_m="$2"
-SP_M="$3"
-SI_T="$4"
-P_T="$5"
-X_="$6"
-Y_="$7"
-MR_U="$8"
+NN="$1" SP_m="$2" SP_M="$3" SI_T="$4" P_T="$5" X_="$6" Y_="$7" MR_U="$8"
 if [ -z "$NN" ]; then
-  echo echo "USAGE: <Nº NODES> <MIN_Speed> <MAX_Speed> <TIME> <PAUSE> <X> <Y> <MIN U_Routes>"
+  echo "USAGE: <Nº NODES> <MIN_Speed> <MAX_Speed> <TIME> <PAUSE> <X> <Y> <MIN U_Routes>"
   exit 1
 fi
 if [ -z "$SP_m" ]; then
@@ -43,9 +36,7 @@ if [ -z "$MR_U" ]; then
   echo "USAGE: <Nº NODES> <MIN_Speed> <MAX_Speed> <TIME> <PAUSE> <X> <Y> <MIN U_Routes>"
   exit 1
 fi
-
 export n=$MR_U
-
 while [ $n -ge $MR_U ]
 do
 ./setdest -v2 -n$NN -m$SP_m -M$SP_M -t$SI_T -p$P_T -x$X_ -y$Y_ > mobility.tcl
