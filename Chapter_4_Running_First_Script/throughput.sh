62007e5e42b42856b6266f20dad1d48f71ea09c3
Diógenes Antonio Marques José <32777186+dioxfile@users.noreply.github.com>
2022-11-29 20:46:21 -0400

    Add files via upload

---
+++ Chapter_4_Running_First_Script/throughput.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cat saida.tr | awk -F " " 'BEGIN {
+fromNode=1; toNode=2;
+lineCount = 0;totalBits = 0;
+}
+/^r/&&$3==fromNode&&$4==toNode {
+    totalBits += 8*$6;
+    if ( lineCount==0 ) {
+        timeBegin = $2; lineCount++;
+    } else {
+        timeEnd = $2;
+    };
+};
+
+END{
+    duration = timeEnd-timeBegin;
+    print "Number of records is " NR;
+    print "Output: ";
+    print "Transmission: N" fromNode "->N" toNode; 
+    print "  - Total transmitted bits = " totalBits " bits";
+    print "  - duration = " duration " s"; 
+    print "  - Thoughput = "  totalBits/duration/1e3 " kbps."; 
+};'
